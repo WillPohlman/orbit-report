cc076696872ceb18329b5a202acdf21889fcc616
William Pohlman <william.a.pohlman@gmail.com>
2020-08-01 17:49:04 -0500

    Bonus Mission C Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,7 +38,13 @@ export class AppComponent {
     searchTerm = searchTerm.toLowerCase();
     for(let i=0; i < this.sourceList.length; i++) {
        let name = this.sourceList[i].name.toLowerCase();
+       let orbitType = this.sourceList[i].orbitType.toLowerCase();
+       let type = this.sourceList[i].type.toLowerCase();
        if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }else if(orbitType.indexOf(searchTerm) >= 0){
+          matchingSatellites.push(this.sourceList[i]);
+       }else if(type.indexOf(searchTerm) >= 0){
           matchingSatellites.push(this.sourceList[i]);
        }
     }
